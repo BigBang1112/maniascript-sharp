e026a0be24e7b319001024ef5c0981aeb98092d6
Petr Pivoňka <petrpiv1@gmail.com>
2022-08-13 06:18:31 +0200

    Add Comment and Enum tests

---
+++ ManiaScriptSharp.DocH.Tests/Unit/Blocks/CommentHBlockTests.cs
@@ -0,0 +1,182 @@
+﻿using ManiaScriptSharp.DocH.Blocks;
+using ManiaScriptSharp.DocH.Inlines;
+using ManiaScriptSharp.DocH.Tests.Mocks;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace ManiaScriptSharp.DocH.Tests.Unit.Blocks;
+
+public class CommentHBlockTests
+{
+    [Fact]
+    public void Start_ReturnsCorrect()
+    {
+        // Arrange
+        var hBlock = new CommentHBlock(depth: 0);
+
+        // Act & Assert
+        Assert.Equal(expected: "/*!", actual: hBlock.Start);
+    }
+
+    [Fact]
+    public void End_ReturnsCorrect()
+    {
+        // Arrange
+        var hBlock = new CommentHBlock(depth: 0);
+
+        // Act & Assert
+        Assert.Equal(expected: "*/", actual: hBlock.End);
+    }
+
+    [Fact]
+    public void UseEmptyLines_IsTrue()
+    {
+        // Arrange
+        var hBlock = new CommentHBlock(depth: 0);
+
+        // Act & Assert
+        Assert.True(hBlock.UseEmptyLines);
+    }
+
+    [Fact]
+    public void ReadLine_AddsCommentCorretly_HandlesAsterisk()
+    {
+        // Arrange
+        var hBlock = new CommentHBlock(depth: 0);
+
+        var exampleString = @"* This is the base Manialink page interface.
+*
+* Supported declare modes :
+* - Local
+* - Persistent
+*/";
+
+        using var exampleStream = new MemoryStream(Encoding.UTF8.GetBytes(exampleString));
+        using var reader = new StreamReader(exampleStream);
+        var builder = new StringBuilder();
+
+        // Act
+        hBlock.ReadLine(reader.ReadLine()!, reader, builder);
+
+        // Assert
+        Assert.True(hBlock.Comments.Count == 1);
+        Assert.Contains(expected: "This is the base Manialink page interface.", hBlock.Comments);
+    }
+
+    [Fact]
+    public void ReadLine_AddsCommentCorretly_HandlesBrief()
+    {
+        // Arrange
+        var hBlock = new CommentHBlock(depth: 0);
+
+        var exampleString = @"* \brief This is the base Manialink page interface.
+*
+* Supported declare modes :
+* - Local
+* - Persistent
+*/";
+        
+        using var exampleStream = new MemoryStream(Encoding.UTF8.GetBytes(exampleString));
+        using var reader = new StreamReader(exampleStream);
+        var builder = new StringBuilder();
+
+        // Act
+        hBlock.ReadLine(reader.ReadLine()!, reader, builder);
+
+        // Assert
+        Assert.True(hBlock.Comments.Count == 1);
+        Assert.Contains(expected: "This is the base Manialink page interface.", hBlock.Comments);
+    }
+
+    [Fact]
+    public void ReadLine_HandlesEmptyLine()
+    {
+        // Arrange
+        var hBlock = new CommentHBlock(depth: 0);
+
+        var exampleString = @"* 
+*
+* Supported declare modes :
+* - Local
+* - Persistent
+*/";
+
+        using var exampleStream = new MemoryStream(Encoding.UTF8.GetBytes(exampleString));
+        using var reader = new StreamReader(exampleStream);
+        var builder = new StringBuilder();
+
+        // Act
+        hBlock.ReadLine(reader.ReadLine()!.Trim(), reader, builder);
+
+        // Assert
+        Assert.True(hBlock.Comments.Count == 1);
+        Assert.Contains(expected: "", hBlock.Comments);
+    }
+
+    [Fact]
+    public void ReadLine_AtLeastOneCommentAlreadyExists()
+    {
+        // Arrange
+        var hBlock = new CommentHBlock(depth: 0);
+        hBlock.Comments.Add("This is the base Manialink page interface.");
+
+        var exampleString = @"
+*
+* Supported declare modes :
+* - Local
+* - Persistent
+*/";
+
+        using var exampleStream = new MemoryStream(Encoding.UTF8.GetBytes(exampleString));
+        using var reader = new StreamReader(exampleStream);
+        var builder = new StringBuilder();
+
+        // Act
+        hBlock.ReadLine(reader.ReadLine()!, reader, builder);
+
+        // Assert
+        Assert.True(hBlock.Comments.Count == 2);
+        Assert.True(hBlock.Comments[1] == "");
+    }
+
+    [Fact]
+    public void AfterRead_CreatesSummaryFromComments()
+    {
+        // Arrange
+        var hBlock = new CommentHBlock(depth: 1);
+        hBlock.Comments.Add("This is the base Manialink page interface.");
+        hBlock.Comments.Add("");
+        hBlock.Comments.Add("Supported declare modes :");
+
+        var expected = $"\t/// <summary>{Environment.NewLine}" +
+            $"\t/// This is the base Manialink page interface.{Environment.NewLine}" +
+            $"\t/// {Environment.NewLine}" +
+            $"\t/// Supported declare modes :{Environment.NewLine}" +
+            $"\t/// </summary>{Environment.NewLine}";
+
+        var builder = new StringBuilder();
+
+        // Act
+        hBlock.AfterRead(builder);
+
+        // Assert
+        Assert.Equal(expected, actual: builder.ToString());
+    }
+
+    [Fact]
+    public void AfterRead_NoComments()
+    {
+        // Arrange
+        var hBlock = new CommentHBlock(depth: 1);
+
+        var expected = $"";
+
+        var builder = new StringBuilder();
+
+        // Act
+        hBlock.AfterRead(builder);
+
+        // Assert
+        Assert.Equal(expected, actual: builder.ToString());
+    }
+}
---
+++ ManiaScriptSharp.DocH.Tests/Unit/Blocks/EnumHBlockTests.cs
@@ -0,0 +1,101 @@
+﻿using ManiaScriptSharp.DocH.Blocks;
+using ManiaScriptSharp.DocH.Inlines;
+using ManiaScriptSharp.DocH.Tests.Mocks;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace ManiaScriptSharp.DocH.Tests.Unit.Blocks;
+
+public class EnumHBlockTests
+{
+    [Fact]
+    public void End_ReturnsCorrect()
+    {
+        // Arrange
+        var hBlock = new MockMajorHBlock();
+
+        // Act
+        var actual = hBlock.End;
+
+        // Assert
+        Assert.Equal(expected: "};", actual);
+    }
+
+    [Fact]
+    public void BeforeRead_MatchNull_Throws()
+    {
+        // Arrange
+        var hBlock = new EnumHBlock();
+        var builder = new StringBuilder();
+
+        // Act & Assert
+        Assert.Throws<Exception>(() => hBlock.BeforeRead("enum TestEnum {", match: null, builder));
+    }
+
+    [Fact]
+    public void BeforeRead_StartsCode()
+    {
+        // Arrange
+        var hBlock = new EnumHBlock();
+        var builder = new StringBuilder();
+        var expected = $"\tpublic enum TestEnum{Environment.NewLine}\t{{{Environment.NewLine}";
+        var exampleString = "enum TestEnum {";
+        var match = hBlock.IdentifierRegex!.Match(exampleString);
+
+        // Act
+        hBlock.BeforeRead(exampleString, match, builder);
+
+        // Assert
+        Assert.Equal(expected, actual: builder.ToString());
+    }
+
+    [Fact]
+    public void ReadLine_CanProcessEnumValue()
+    {
+        // Arrange
+        var hBlock = new EnumHBlock();
+        var expected = $"\t\tOne,{Environment.NewLine}";
+        var exampleString = @"One,";
+        using var exampleStream = new MemoryStream(Encoding.UTF8.GetBytes(exampleString));
+        using var reader = new StreamReader(exampleStream);
+        var builder = new StringBuilder();
+
+        // Act
+        hBlock.ReadLine(reader.ReadLine()!, reader, builder);
+
+        // Assert
+        Assert.Equal(expected, actual: builder.ToString());
+    }
+
+    [Fact]
+    public void ReadLine_CanProcessEnumValue_ForbiddenChars()
+    {
+        // Arrange
+        var hBlock = new EnumHBlock();
+        var expected = $"\t\t[ActualName(\"(One)\")] _One_,{Environment.NewLine}";
+        var exampleString = @"(One),";
+        using var exampleStream = new MemoryStream(Encoding.UTF8.GetBytes(exampleString));
+        using var reader = new StreamReader(exampleStream);
+        var builder = new StringBuilder();
+
+        // Act
+        hBlock.ReadLine(reader.ReadLine()!, reader, builder);
+
+        // Assert
+        Assert.Equal(expected, actual: builder.ToString());
+    }
+
+    [Fact]
+    public void AfterRead_AppendsEndCurlyBracket()
+    {
+        // Arrange
+        var hBlock = new EnumHBlock();
+        var builder = new StringBuilder();
+
+        // Act
+        hBlock.AfterRead(builder);
+
+        // Assert
+        Assert.Equal($"\t}}{Environment.NewLine}{Environment.NewLine}", builder.ToString());
+    }
+}
