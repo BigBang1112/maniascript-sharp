7ed4cf0d32a38e66e08f9bcdd15c17fc88e18c49
Petr Pivoňka <petrpiv1@gmail.com>
2022-09-29 01:14:08 +0200

    Update sample

---
+++ Sample/Gamemode.cs
@@ -0,0 +1,18 @@
+﻿namespace Sample;
+
+public class Gamemode : CTmMode, IContext
+{
+    public struct Player
+    {
+        public string Text;
+        public string Icon;
+    }
+
+    [Setting(As = "Chat time")]
+    public const int ChatTime = 50;
+
+    public void Execute()
+    {
+        Log("bro");
+    }
+}
