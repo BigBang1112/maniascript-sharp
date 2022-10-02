5afd65be58ac21c6108f4ae3412f4b94c9cbaf83
Petr Pivoňka <petrpiv1@gmail.com>
2022-10-02 20:58:00 +0200

    Fix unit test

--- ManiaScriptSharp.DocH.Tests/Mocks/MockAttributeData.cs
+++ ManiaScriptSharp.DocH.Tests/Mocks/MockAttributeData.cs
@@ -8,7 +8,7 @@ internal class MockAttributeData : AttributeData
     protected override INamedTypeSymbol? CommonAttributeClass { get; }
     protected override IMethodSymbol? CommonAttributeConstructor => throw new NotImplementedException();
     protected override SyntaxReference? CommonApplicationSyntaxReference => throw new NotImplementedException();
-    protected override ImmutableArray<TypedConstant> CommonConstructorArguments => throw new NotImplementedException();
+    protected override ImmutableArray<TypedConstant> CommonConstructorArguments { get; } = ImmutableArray<TypedConstant>.Empty;
     protected override ImmutableArray<KeyValuePair<string, TypedConstant>> CommonNamedArguments => throw new NotImplementedException();
 
     public MockAttributeData(INamedTypeSymbol attributeClass)
