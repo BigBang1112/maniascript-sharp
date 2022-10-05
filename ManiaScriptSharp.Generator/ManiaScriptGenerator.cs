b8bbba42290df994f01094c4ed699d2c43ad3697
Petr Pivoňka <petrpiv1@gmail.com>
2022-10-05 09:30:33 +0200

    Default output directory to out

--- ManiaScriptSharp.Generator/ManiaScriptGenerator.cs
+++ ManiaScriptSharp.Generator/ManiaScriptGenerator.cs
@@ -24,8 +24,11 @@ public class ManiaScriptGenerator : ISourceGenerator
         {
             throw new Exception("build_property.projectdir not found");
         }
+        
+        var outputDir = Path.Combine(projectDir, "out");
 
-        var outputDir = @""; // E:\Temp\ManiaScriptSharp
+        var fileSystem = new FileSystem();
+        fileSystem.Directory.CreateDirectory(outputDir);
 
         var scriptSymbols = context.Compilation
             .GlobalNamespace
@@ -33,8 +36,6 @@ public class ManiaScriptGenerator : ISourceGenerator
             .SelectMany(x => x.GetTypeMembers()
                 .Where(y => y.Interfaces.Any(z => z.Name == "IContext")))
             .ToList();
-        
-        var fileSystem = new FileSystem();
         
         foreach (var scriptSymbol in scriptSymbols)
         {
@@ -44,24 +45,28 @@ public class ManiaScriptGenerator : ISourceGenerator
 
     private static IGeneratedFile GenerateScriptFile(INamedTypeSymbol scriptSymbol, IFileSystem fileSystem, string projectDir, string outputDir)
     {
-        var outputFilePath = Path.Combine(outputDir, scriptSymbol.Name);
-        var isInMemory = string.IsNullOrWhiteSpace(outputDir);
         var isEmbeddedScript = scriptSymbol.IsSubclassOf(x => x.Name == "CMlScript");
+        var outputFilePath = Path.Combine(outputDir, scriptSymbol.Name);
+
+        outputFilePath += isEmbeddedScript ? ".xml" : ".Script.txt";
+
+        using var writer = fileSystem.File.CreateText(outputFilePath);
+
+        return GenerateScriptFile(scriptSymbol, fileSystem, projectDir, writer, isEmbeddedScript);
+    }
+
+    private static IGeneratedFile GenerateScriptFile(INamedTypeSymbol scriptSymbol,
+                                                     IFileSystem fileSystem,
+                                                     string projectDir,
+                                                     TextWriter writer,
+                                                     bool isEmbeddedScript)
+    {
+        
 
         if (!isEmbeddedScript)
         {
             // All regular scripts go here (.Script.txt)
-
-            if (isInMemory)
-            {
-                using var scriptInMemoryWriter = new StringWriter();
-                
-                return ManiaScriptFile.Generate(scriptSymbol, scriptInMemoryWriter);
-            }
-            
-            using var scriptWriter = fileSystem.File.CreateText(outputFilePath + ".Script.txt");
-            
-            return ManiaScriptFile.Generate(scriptSymbol, scriptWriter);
+            return ManiaScriptFile.Generate(scriptSymbol, writer);
         }
         
         // Manialink work goes here (.xml)
@@ -69,17 +74,8 @@ public class ManiaScriptGenerator : ISourceGenerator
         _ = projectDir ?? throw new InvalidOperationException("ProjectDirectory must be set for manialink builds.");
 
         var xml = ReadManialinkXml(scriptSymbol, fileSystem, projectDir);
-        
-        if (isInMemory)
-        {
-            using var manialinkInMemoryWriter = new StringWriter();
-            
-            return ManialinkFile.Generate(xml, scriptSymbol, manialinkInMemoryWriter);
-        }
 
-        using var manialinkWriter = fileSystem.File.CreateText(outputFilePath + ".xml");
-
-        return ManialinkFile.Generate(xml, scriptSymbol, manialinkWriter);
+        return ManialinkFile.Generate(xml, scriptSymbol, writer);
     }
 
     private static string ReadManialinkXml(INamedTypeSymbol scriptSymbol, IFileSystem fileSystem, string projectDir)
