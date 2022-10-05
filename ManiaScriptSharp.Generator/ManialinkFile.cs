3582739798ba7b2f7d1c11163797df185591bf70
Petr Pivoňka <petrpiv1@gmail.com>
2022-10-05 13:07:16 +0200

    Build manialink a bit better

--- ManiaScriptSharp.Generator/ManialinkFile.cs
+++ ManiaScriptSharp.Generator/ManialinkFile.cs
@@ -1,4 +1,6 @@
 using Microsoft.CodeAnalysis;
+using System.Text;
+using System.Xml;
 
 namespace ManiaScriptSharp.Generator;
 
@@ -6,11 +8,33 @@ public class ManialinkFile : IGeneratedFile
 {
     public static ManialinkFile Generate(string xml, INamedTypeSymbol scriptSymbol, TextWriter writer)
     {
-        writer.WriteLine(xml);
+        var doc = new XmlDocument();
+        doc.LoadXml(xml);
+
+        using var scriptWriter = new StringWriter();
+
+        var newLineCount = 2;
+
+        for (int i = 0; i < newLineCount; i++)
+        {
+            scriptWriter.WriteLine();
+        }
+
+        var headBuilder = new ManiaScriptHeadBuilder(scriptSymbol, scriptWriter, isEmbeddedInManialink: true);
+        var head = headBuilder.AnalyzeAndBuild();
+
+        // Trick to check if the script is empty, compatible with any kind of new line, will change in the future
+        if (scriptWriter.GetStringBuilder().Length > scriptWriter.NewLine.Length * 2)
+        {
+            var scriptCData = doc.CreateCDataSection(scriptWriter.ToString());
+
+            var scriptElement = doc.CreateElement("script");
+            scriptElement.AppendChild(scriptCData);
+
+            doc.DocumentElement.AppendChild(scriptElement);
+        }
         
-        var headBuilder = new ManiaScriptHeadBuilder(scriptSymbol, writer, isEmbeddedInManialink: true);
-        
-        headBuilder.AnalyzeAndBuild();
+        doc.Save(writer);
 
         return new ManialinkFile();
     }
