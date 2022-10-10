64aee668213f030e3dcdc1345c52b1a971a4c3e4
Petr Pivoňka <petrpiv1@gmail.com>
2022-10-10 22:40:44 +0200

    Support this

--- Sample/Gamemode.cs
+++ Sample/Gamemode.cs
@@ -11,12 +11,31 @@ public class Gamemode : CTmMode, IContext
     [Setting(As = "Chat time")]
     public const int ChatTime = 50;
 
+    public int Ggsdg = 69;
+
     public void SpawnAllWaitingPlayers(int team, int raceStartTime)
     {
         foreach (var player in PlayersWaiting)
         {
             SpawnPlayer(player, team, raceStartTime);
         }
+    }
+
+    protected override void OnPendingEvent(CTmModeEvent e)
+    {
+        base.OnPendingEvent(e);
+    }
+
+    protected override void OnStartLine(CTmPlayer player)
+    {
+        base.OnStartLine(player);
+    }
+
+    protected override void OnStunt(CTmPlayer player, int raceTime, CTmModeEvent.EStuntFigure stuntFigure, int angle, int points, float factor, int combo,
+        bool isStraight, bool isReverse, bool isPerfectLanding, bool isMasterJump, bool isMasterLanding,
+        bool isEpicLanding)
+    {
+        base.OnStunt(player, raceTime, stuntFigure, angle, points, factor, combo, isStraight, isReverse, isPerfectLanding, isMasterJump, isMasterLanding, isEpicLanding);
     }
 
     public void Main()
