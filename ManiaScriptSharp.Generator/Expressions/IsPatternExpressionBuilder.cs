9785813f91a94b3246119dc4ba67eb233894d1bc
Petr Pivoňka <petrpiv1@gmail.com>
2022-10-15 23:29:51 +0200

    Enhance 'is' pattern

--- ManiaScriptSharp.Generator/Expressions/IsPatternExpressionBuilder.cs
+++ ManiaScriptSharp.Generator/Expressions/IsPatternExpressionBuilder.cs
@@ -7,128 +7,190 @@ namespace ManiaScriptSharp.Generator.Expressions;
 
 public class IsPatternExpressionBuilder : ExpressionBuilder<IsPatternExpressionSyntax>
 {
-    public override void Write(int ident, IsPatternExpressionSyntax expression, ImmutableDictionary<string, ParameterSyntax> parameters,
-        ManiaScriptBodyBuilder bodyBuilder)
+    public override void Write(int ident, IsPatternExpressionSyntax expression,
+        ImmutableDictionary<string, ParameterSyntax> parameters, ManiaScriptBodyBuilder bodyBuilder)
     {
-        WriteSyntax(ident, expression.Expression, parameters, bodyBuilder);
-        
-        if (expression.Pattern is DeclarationPatternSyntax declarationPatternSyntax)
+        WritePattern(expression.Pattern,
+            expression.Expression,
+            new[] {expression.Expression},
+            new Dictionary<ExpressionSyntax, TypeSyntax>());
+
+        void WritePattern(PatternSyntax pattern, ExpressionSyntax currentExpression, ExpressionSyntax[] expressions,
+            Dictionary<ExpressionSyntax, TypeSyntax> types)
         {
-            Writer.Write(" is ");
-            WriteSyntax(ident, declarationPatternSyntax.Type, parameters, bodyBuilder);
-
-            WriteDesignation(declarationPatternSyntax.Designation, declarationPatternSyntax.Type);
-        }
-        else if (expression.Pattern is RecursivePatternSyntax recursivePatternSyntax)
-        {
-            var typeCheckCompleted = false;
-            
-            if (recursivePatternSyntax.Type is not null)
+            switch (pattern)
             {
-                Writer.Write(" is ");
-                WriteSyntax(ident, recursivePatternSyntax.Type, parameters, bodyBuilder);
-                typeCheckCompleted = true;
-            }
-
-            if (recursivePatternSyntax.PropertyPatternClause is not null)
-            {
-                foreach (var subpattern in recursivePatternSyntax.PropertyPatternClause.Subpatterns)
-                {
-                    if (typeCheckCompleted)
+                case RecursivePatternSyntax recursivePatternSyntax:
+                    
+                    if (recursivePatternSyntax.Type is not null)
                     {
-                        Writer.Write(" && ");
-                    }
-
-                    if (subpattern.NameColon is not null)
-                    {
-                        if (typeCheckCompleted)
-                        {
-                            WriteSyntax(ident, expression.Expression, parameters, bodyBuilder);
-                        }
-
-                        Writer.Write('.');
+                        WriteExpression(ident, parameters, bodyBuilder, expressions, types);
+                        Writer.Write(" is ");
+                        WriteSyntax(ident, recursivePatternSyntax.Type, parameters, bodyBuilder);
                         
-                        WriteSyntax(ident, subpattern.NameColon.Expression, parameters, bodyBuilder);
-
-                        Writer.Write(' ');
-                        
-                        switch (subpattern.Pattern)
-                        {
-                            case ConstantPatternSyntax constantPatternSyntax:
-                                Writer.Write("== ");
-                                WriteSyntax(ident, constantPatternSyntax.Expression, parameters, bodyBuilder);
-                                break;
-                            case RelationalPatternSyntax relationalPatternSyntax:
-                                Writer.Write(relationalPatternSyntax.OperatorToken.Text);
-                                Writer.Write(' ');
-                                WriteSyntax(ident, relationalPatternSyntax.Expression, parameters, bodyBuilder);
-                                break;
-                            case BinaryPatternSyntax binaryPatternSyntax:
-
-                                break;
-                            default:
-                                Writer.Write("/* ");
-                                Writer.Write(subpattern.Pattern.GetType().Name);
-                                Writer.Write(" */");
-                                break;
-                        }
+                        types[currentExpression] = recursivePatternSyntax.Type;
                     }
                     
-                    typeCheckCompleted = true;
-                }
-            }
+                    if (recursivePatternSyntax.PropertyPatternClause is not null)
+                    {
+                        for (var i = 0; i < recursivePatternSyntax.PropertyPatternClause.Subpatterns.Count; i++)
+                        {
+                            var subpattern = recursivePatternSyntax.PropertyPatternClause.Subpatterns[i];
+
+                            if (subpattern.Pattern is RecursivePatternSyntax {PropertyPatternClause.Subpatterns.Count: 0})
+                            {
+                                if (subpattern.NameColon is null)
+                                {
+                                    Writer.Write("/* NameColon is null */");
+                                }
+                                else
+                                {
+                                    WritePattern(subpattern.Pattern,
+                                        subpattern.NameColon.Expression,
+                                        expressions.Append(subpattern.NameColon.Expression).ToArray(),
+                                        types);
+                                }
+
+                                continue;
+                            }
+                            
+                            if (recursivePatternSyntax.Type is not null || i != 0)
+                            {
+                                Writer.Write(" && ");
+                            }
+                            
+                            if (subpattern.NameColon is null)
+                            {
+                                Writer.Write("/* NameColon is null */");
+                                continue;
+                            }
+
+                            Writer.Write('(');
+
+                            WritePattern(subpattern.Pattern,
+                                subpattern.NameColon.Expression,
+                                expressions.Append(subpattern.NameColon.Expression).ToArray(),
+                                types);
+
+                            Writer.Write(')');
+                        }
+                    }
             
-            if (recursivePatternSyntax.Designation is not null)
-            {
-                WriteDesignation(recursivePatternSyntax.Designation, recursivePatternSyntax.Type);
+                    if (recursivePatternSyntax.Designation is not null)
+                    {
+                        WriteDesignation(recursivePatternSyntax.Designation, expressions, types);
+                    }
+                    break;
+                case DeclarationPatternSyntax declarationPatternSyntax:
+                    WriteExpression(ident, parameters, bodyBuilder, expressions, types);
+                    Writer.Write(" is ");
+                    WriteSyntax(ident, declarationPatternSyntax.Type, parameters, bodyBuilder);
+                    types[currentExpression] = declarationPatternSyntax.Type;
+                    WriteDesignation(declarationPatternSyntax.Designation, expressions, types);
+                    break;
+                case ConstantPatternSyntax constantPatternSyntax:
+                    WriteExpression(ident, parameters, bodyBuilder, expressions, types);
+                    Writer.Write(" == ");
+                    WriteSyntax(ident, constantPatternSyntax.Expression, parameters, bodyBuilder);
+                    break;
+                case RelationalPatternSyntax relationalPatternSyntax:
+                    WriteExpression(ident, parameters, bodyBuilder, expressions, types);
+                    Writer.Write(' ');
+                    Writer.Write(relationalPatternSyntax.OperatorToken.Text);
+                    Writer.Write(' ');
+                    WriteSyntax(ident, relationalPatternSyntax.Expression, parameters, bodyBuilder);
+                    break;
+                case BinaryPatternSyntax binaryPatternSyntax:
+                    WritePattern(binaryPatternSyntax.Left, currentExpression, expressions, types);
+                    
+                    switch (binaryPatternSyntax.OperatorToken.Text)
+                    {
+                        case "and":
+                            Writer.Write(" && ");
+                            break;
+                        case "or":
+                            Writer.Write(" || ");
+                            break;
+                    }
+                    
+                    WritePattern(binaryPatternSyntax.Right, currentExpression, expressions, types);
+                    
+                    break;
+                case ParenthesizedPatternSyntax parenthesizedPatternSyntax:
+                    Writer.Write('(');
+                    WritePattern(parenthesizedPatternSyntax.Pattern, currentExpression, expressions, types);
+                    Writer.Write(')');
+                    break;
+                case UnaryPatternSyntax unaryPatternSyntax:
+                    Writer.Write("!(");
+                    WritePattern(unaryPatternSyntax.Pattern, currentExpression, expressions, types);
+                    Writer.Write(')');
+                    break;
+                default:
+                    Writer.Write("/* ");
+                    Writer.Write(pattern.GetType().Name);
+                    Writer.Write(" */");
+                    break;
             }
-        }
-        else
-        {
-            Writer.Write("/*");
-            Writer.Write(expression.Pattern.GetType().Name);
-            Writer.Write("*/");
-        }
 
-        void WriteDesignation(VariableDesignationSyntax designation, ExpressionSyntax? type)
-        {
-            if (designation is SingleVariableDesignationSyntax singleVariableSyntax)
+            void WriteDesignation(VariableDesignationSyntax designation,
+                IReadOnlyList<ExpressionSyntax> expressionSyntaxes,
+                IReadOnlyDictionary<ExpressionSyntax, TypeSyntax> typeSyntaxes)
             {
-                var lineBuilder = new StringWriter();
-                lineBuilder.Write("declare ");
-                lineBuilder.Write(Standardizer.StandardizeName(singleVariableSyntax.Identifier.Text));
-
-                lineBuilder.Write(" = ");
-                
-                if (type is not null)
+                if (designation is SingleVariableDesignationSyntax singleVariableSyntax)
                 {
-                    lineBuilder.Write('(');
-                }
+                    var lineBuilder = new StringWriter();
+                    lineBuilder.Write("declare ");
+                    lineBuilder.Write(Standardizer.StandardizeName(singleVariableSyntax.Identifier.Text));
 
-                WriteSyntax(ident, expression.Expression, parameters,
-                    new ManiaScriptBodyBuilder(bodyBuilder.ScriptSymbol, bodyBuilder.SemanticModel, lineBuilder,
-                        bodyBuilder.Head, bodyBuilder.Helper));
-
-                if (type is not null)
-                {
-                    lineBuilder.Write(" as ");
-
-                    WriteSyntax(ident, type, parameters,
+                    lineBuilder.Write(" = ");
+                    
+                    WriteExpression(ident, parameters,
                         new ManiaScriptBodyBuilder(bodyBuilder.ScriptSymbol, bodyBuilder.SemanticModel, lineBuilder,
-                            bodyBuilder.Head, bodyBuilder.Helper));
-
-                    lineBuilder.Write(')');
+                            bodyBuilder.Head, bodyBuilder.Helper), expressionSyntaxes, typeSyntaxes);
+                    
+                    lineBuilder.Write(';');
+                    
+                    bodyBuilder.BlockLineQueue.Enqueue(lineBuilder.ToString());
                 }
-                
-                lineBuilder.Write(';');
-
-                bodyBuilder.BlockLineQueue.Enqueue(lineBuilder.ToString());
+                else
+                {
+                    Writer.Write("/*");
+                    Writer.Write(designation.GetType().Name);
+                    Writer.Write("*/");
+                }
             }
-            else
+        }
+    }
+
+    private void WriteExpression(int ident, ImmutableDictionary<string, ParameterSyntax> parameters,
+        ManiaScriptBodyBuilder bodyBuilder, IReadOnlyList<ExpressionSyntax> expressions,
+        IReadOnlyDictionary<ExpressionSyntax, TypeSyntax> types)
+    {
+        foreach (var expression in expressions)
+        {
+            if (types.ContainsKey(expression))
             {
-                Writer.Write("/*");
-                Writer.Write(designation.GetType().Name);
-                Writer.Write("*/");
+                bodyBuilder.Writer.Write('(');
+            }
+        }
+
+        for (var i = 0; i < expressions.Count; i++)
+        {
+            var expressionSyntax = expressions[i];
+                    
+            WriteSyntax(ident, expressionSyntax, parameters, bodyBuilder);
+
+            if (types.TryGetValue(expressionSyntax, out var typeSyntax))
+            {
+                bodyBuilder.Writer.Write(" as ");
+                WriteSyntax(ident, typeSyntax, parameters, bodyBuilder);
+                bodyBuilder.Writer.Write(')');
+            }
+
+            if (i != expressions.Count - 1)
+            {
+                bodyBuilder.Writer.Write('.');
             }
         }
     }
