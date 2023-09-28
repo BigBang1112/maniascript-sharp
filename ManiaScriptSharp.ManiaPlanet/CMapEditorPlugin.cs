cee1b3ad477d80fc54cf026429aeb47160bfc529
Petr Pivoňka <petrpiv1@gmail.com>
2023-09-29 00:38:33 +0200

    Add some CMapEditorPlugin events

---
+++ ManiaScriptSharp.ManiaPlanet/CMapEditorPlugin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Immutable;
+
+namespace ManiaScriptSharp;
+
+public partial class CMapEditorPlugin
+{
+    /// <summary>
+    /// Any pending event handler.
+    /// </summary>
+    /// <param name="e">Event.</param>
+    [ManiaScriptEvent(nameof(PendingEvents))]
+    public delegate void PendingEventHandler(CMapEditorPluginEvent e);
+    
+    [ManiaScriptEvent(nameof(PendingEvents))]
+    public delegate void StartTestEventHandler();
+}
